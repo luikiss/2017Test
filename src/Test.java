7e5ec5da481583b06e9a00624f4e830f258719f8
liuyongxin <619193848@qq.com>
2017-03-30 15:35:49 +0800

    first test

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.printf("someting care速度"
-        );
+        for(int x = 1; x <= 9; x++)
+        {
+            for(int y = 1; y <= x; y++)
+            {
+                System.out.print(y+"*"+x+"="+x*y+"\t");
+            }
+            System.out.println();
+        }
     }
 }
