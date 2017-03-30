2be7f7438dc583ee6a31f2af37a56fcf1da2a502
liuyongxin <619193848@qq.com>
2017-03-30 15:38:20 +0800

    gogogo

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        for(int x = 1; x <= 9; x++)
-        {
-            for(int y = 1; y <= x; y++)
-            {
-                System.out.print(y+"*"+x+"="+x*y+"\t");
-            }
-            System.out.println();
-        }
+        System.out.printf("gogogo");
     }
 }
