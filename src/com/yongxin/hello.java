15d08f882efc77dc43b435723b8ad51ad97fc861
liuyongxin <yongzn@163.com>
2017-04-26 22:52:29 +0800

    test4.26 22:51

---
+++ src/com/yongxin/hello.java
@@ -0,0 +1,16 @@
+package com.yongxin;
+
+/**
+ * Created by yongxin on 2017/4/26.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.printf("test");
+        System.out.printf("test");
+        System.out.printf("test");
+        System.out.printf("test");
+        System.out.printf("test");
+        System.out.printf("test");
+
+    }
+}
